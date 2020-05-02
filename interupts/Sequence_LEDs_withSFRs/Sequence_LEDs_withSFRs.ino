40d5f6467f89280d88f329ce25f6367b911612ca
cstone92 <51446825+cstone92@users.noreply.github.com>
2020-05-02 14:06:01 -0400

    hardwear check complete

--- interupts/Sequence_LEDs_withSFRs/Sequence_LEDs_withSFRs.ino
+++ interupts/Sequence_LEDs_withSFRs/Sequence_LEDs_withSFRs.ino
@@ -7,16 +7,41 @@
 #define PIN_PUSHBUTTON_YELLOW 2
 #define PIN_PUSHBUTTON_GREEN 1
 #define PIN_PUSHBUTTON_BLUE 0
+
+// Output PORTs and BITs
+#define REG_PORT_LED_RED PORTB
+#define BIT_LED_RED 2
+#define REG_PORT_LED_YELLOW PORTB
+#define BIT_LED_YELLOW 1
+#define REG_PORT_LED_GREEN PORTD
+#define BIT_LED_GREEN 6
+#define REG_PORT_LED_BLUE PORTD
+#define BIT_LED_BLUE 5
+
+// Input PORTs and PIN reg and BITs
+#define REG_PORT_PUSHBUTTON_RED PORTD
+#define REG_PIN_PUSHBUTTON_RED PIND
+#define BIT_PUSHBUTTON_RED 3
+#define REG_PORT_PUSHBUTTON_YELLOW PORTD
+#define REG_PIN_PUSHBUTTON_YELLOW PIND
+#define BIT_PUSHBUTTON_YELLOW 2
+#define REG_PORT_PUSHBUTTON_GREEN PORTD
+#define REG_PIN_PUSHBUTTON_GREEN PIND
+#define BIT_PUSHBUTTON_GREEN 1
+#define REG_PORT_PUSHBUTTON_BLUE PORTD
+#define REG_PIN_PUSHBUTTON_BLUE PIND
+#define BIT_PUSHBUTTON_BLUE 0
+
 #define ON 1
 #define OFF 0
 
 #define PRESSED 0
 #define NOTPRESSED 1
 
-uint8_t blueState = LOW;
-uint8_t lastBlueState = LOW;
-uint8_t greenState = LOW;
-uint8_t lastGreenState = LOW;
+uint8_t blueState = HIGH;
+uint8_t lastBlueState = HIGH;
+uint8_t greenState = HIGH;
+uint8_t lastGreenState = HIGH;
 
 uint8_t currentIndex = 0;
 uint8_t savedLeds[10] = {PIN_LED_BLUE,
@@ -37,22 +62,58 @@ volatile uint8_t mainEventFlags = 0;
 void setup() {
   Serial.begin(9600);
 
-  pinMode(PIN_LED_RED, OUTPUT);
-  pinMode(PIN_LED_YELLOW, OUTPUT);
-  pinMode(PIN_LED_GREEN, OUTPUT);
-  pinMode(PIN_LED_BLUE, OUTPUT);
+  DDRB |= _BV(BIT_LED_RED);     //pinMode(PIN_LED_RED, OUTPUT);  // output = 1 input = 0
+  DDRB |= _BV(BIT_LED_YELLOW);  //pinMode(PIN_LED_YELLOW, OUTPUT);
+  DDRD |= _BV(BIT_LED_GREEN);   //pinMode(PIN_LED_GREEN, OUTPUT);
+  DDRD |= _BV(BIT_LED_BLUE);    //pinMode(PIN_LED_BLUE, OUTPUT);
+  // all other bits are left as input which is default
 
+  // enable the pullup resistors
+  REG_PORT_PUSHBUTTON_RED |= _BV(BIT_PUSHBUTTON_RED);
+  REG_PORT_PUSHBUTTON_YELLOW |= _BV(BIT_PUSHBUTTON_YELLOW);
+  REG_PORT_PUSHBUTTON_GREEN |= _BV(BIT_PUSHBUTTON_GREEN);
+  REG_PORT_PUSHBUTTON_BLUE |= _BV(BIT_PUSHBUTTON_BLUE);
 
-  pinMode(PIN_PUSHBUTTON_RED, INPUT_PULLUP);
-  pinMode(PIN_PUSHBUTTON_YELLOW, INPUT_PULLUP);
-  pinMode(PIN_PUSHBUTTON_GREEN, INPUT_PULLUP);
-  pinMode(PIN_PUSHBUTTON_BLUE, INPUT_PULLUP);
-
+  // update this later
   attachInterrupt(digitalPinToInterrupt(PIN_PUSHBUTTON_YELLOW), yellow_pushbutton_isr, FALLING);
   attachInterrupt(digitalPinToInterrupt(PIN_PUSHBUTTON_RED), red_pushbutton_isr, FALLING);
 }
 
 void loop() {
+  if (bit_is_clear(REG_PIN_PUSHBUTTON_RED, BIT_PUSHBUTTON_RED)) {
+    REG_PORT_LED_RED |= _BV(BIT_LED_RED);
+  } else {
+    REG_PORT_LED_RED &= ~_BV(BIT_LED_RED);
+  }
+  if (bit_is_clear(REG_PIN_PUSHBUTTON_YELLOW, BIT_PUSHBUTTON_YELLOW)) {
+    REG_PORT_LED_YELLOW |= _BV(BIT_LED_YELLOW);
+  } else {
+    REG_PORT_LED_YELLOW &= ~_BV(BIT_LED_YELLOW);
+  }
+
+  if (bit_is_clear(REG_PIN_PUSHBUTTON_GREEN, BIT_PUSHBUTTON_GREEN)) {
+    REG_PORT_LED_GREEN |= _BV(BIT_LED_GREEN);
+  } else {
+    REG_PORT_LED_GREEN &= ~_BV(BIT_LED_GREEN);
+  }
+
+  if (bit_is_clear(REG_PIN_PUSHBUTTON_BLUE, BIT_PUSHBUTTON_BLUE)) {
+    REG_PORT_LED_BLUE |= _BV(BIT_LED_BLUE);
+  } else {
+    REG_PORT_LED_BLUE &= ~_BV(BIT_LED_BLUE);
+  }
+  
+}
+
+void oldTestLoop() {
+  // Test temp loop function
+  REG_PORT_LED_RED |= _BV(BIT_LED_RED);
+  REG_PORT_LED_YELLOW |= _BV(BIT_LED_YELLOW);
+  REG_PORT_LED_GREEN |= _BV(BIT_LED_GREEN);
+  REG_PORT_LED_BLUE |= _BV(BIT_LED_BLUE);
+}
+
+void loop2() {
   // give feedback when a button is pressed
   digitalWrite(PIN_LED_RED, !digitalRead(PIN_PUSHBUTTON_RED));
   digitalWrite(PIN_LED_YELLOW, !digitalRead(PIN_PUSHBUTTON_YELLOW));
